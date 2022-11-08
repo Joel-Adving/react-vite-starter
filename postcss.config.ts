7da94cadaef12564ff9ac8a9cd76f61b9910e591
Joel Adving <joel.adving@gmail.com>
2022-11-08 23:32:22 +0100

    refactoring

--- postcss.config.ts
+++ postcss.config.ts
@@ -2,9 +2,9 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     'postcss-preset-env': {},
-    'postcss-custom-media': {},
     'postcss-nested': {},
     'postcss-nesting': {},
+    'postcss-custom-media': {},
     autoPrefixer: {},
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import postcssNested from 'postcss-nested';
 import path from 'path';
+
+import postcssNested from 'postcss-nested';
+import postcssNesting from 'postcss-nesting';
+import postcssCustomMedia from 'postcss-custom-media';
+import postcssImport from 'postcss-import';
+import postcssPresetEnv from 'postcss-preset-env';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +22,7 @@ export default defineConfig({
   },
   css: {
     postcss: {
-      plugins: [postcssNested],
+      plugins: [postcssImport, postcssPresetEnv, postcssCustomMedia, postcssNesting, postcssNested, autoprefixer],
     },
   },
   test: {
